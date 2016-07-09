3e5e13bc796b87fd0e42732ae4cea3a49e699ec0
rivneglee <chengli@thoughtworks.com>
2016-07-09 22:48:53 +0800

    add expection classes

---
+++ src/main/java/com/stardust/easyassess/core/exception/ESAppException.java
@@ -0,0 +1,7 @@
+package com.stardust.easyassess.core.exception;
+
+public class ESAppException extends Exception {
+    public ESAppException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/stardust/easyassess/core/exception/MessageException.java
@@ -0,0 +1,7 @@
+package com.stardust.easyassess.core.exception;
+
+public class MessageException extends ESAppException {
+    public MessageException(String error) {
+        super(error);
+    }
+}
